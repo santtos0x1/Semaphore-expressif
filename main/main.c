de6d7b9357d278c336493b9fcbc13623c15f2e47
Santtos <ruanrodrigues393@gmail.com>
2026-01-23 08:41:39 -0300

    Commenting and revising

--- main/main.c
+++ main/main.c
@@ -2,11 +2,14 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_timer.h"
+#include "esp_timer.h" // interrupts & ISR
 //#include "esp_log.h"
 
+// defining the max size of the stack for the task
 #define STACK_SIZE 2048
 
+
+// config of the led pinout
 gpio_config_t red_led_io_conf = {
     // 00000...000000010000000000000000
     .pin_bit_mask = (1ULL << 16),
@@ -16,6 +19,7 @@ gpio_config_t red_led_io_conf = {
     .intr_type = GPIO_INTR_DISABLE
 };
 
+// config of the led pinout
 gpio_config_t yel_led_io_conf = {
     // 00000...000000010000000000000000
     .pin_bit_mask = (1ULL << 21),
@@ -25,6 +29,7 @@ gpio_config_t yel_led_io_conf = {
     .intr_type = GPIO_INTR_DISABLE
 };
 
+// config of the led pinout
 gpio_config_t grn_led_io_conf = {
     // 00000...000000010000000000000000
     .pin_bit_mask = (1ULL << 19),
@@ -34,6 +39,7 @@ gpio_config_t grn_led_io_conf = {
     .intr_type = GPIO_INTR_DISABLE
 };
 
+// config of the button pinout
 gpio_config_t btn_io_conf = {
     .pin_bit_mask = (1ULL << 18),
     .mode = GPIO_MODE_INPUT,
@@ -42,6 +48,7 @@ gpio_config_t btn_io_conf = {
     .intr_type = GPIO_INTR_NEGEDGE
 };
 
+// creating the struct to send for the taskCode as parameter
 typedef struct {
     uint8_t red_led_pin;
     uint8_t yel_led_pin;
@@ -55,12 +62,14 @@ void vTaskCode( void * pvParameters )
 {
     gpio_pinout* pins = ( gpio_pinout* ) pvParameters;
     const TickType_t xHdelay = 300;
+    const TickType_t xLdelay = 100;
 
     // Task
     for( ;; )
     {
         gpio_set_level( ( gpio_num_t )pins->grn_led_pin, 1 );
 
+        // if receives the notification
         if( ulTaskNotifyTake( pdTRUE, portMAX_DELAY ) )
         {
             int64_t now = esp_timer_get_time();
@@ -69,6 +78,11 @@ void vTaskCode( void * pvParameters )
                 last_btn_time = now; 
 
                 gpio_set_level( ( gpio_num_t )pins->grn_led_pin, 0 );
+                gpio_set_level( ( gpio_num_t )pins->yel_led_pin, 1 );
+
+                vTaskDelay( xLdelay );
+
+                gpio_set_level( ( gpio_num_t )pins->yel_led_pin, 0 );
                 gpio_set_level( ( gpio_num_t )pins->red_led_pin, 1 );
                 
                 vTaskDelay( xHdelay );
@@ -80,38 +94,52 @@ void vTaskCode( void * pvParameters )
     }
 }
 
-void IRAM_ATTR vISRHandler(void * args)
-{
+/* 
+   this function is storaged in ram memory for otimization 
+   and notifies the task when the button is pressed
+*/
+void IRAM_ATTR vISRHandler( void * args )
+{   
+    // casting void* to TaskHandle_t
     TaskHandle_t taskToNotify = ( TaskHandle_t ) args;
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-    vTaskNotifyGiveFromISR(taskToNotify, &xHigherPriorityTaskWoken);
-    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    // notify the task when the button is pressed
+    vTaskNotifyGiveFromISR( taskToNotify, &xHigherPriorityTaskWoken );
+    // forces the task to init the new function
+    portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
 }
 
+// this function creates the task and set the notify trigger
 void taskBuilder( void )
 {
+    // defining the pins
     static gpio_pinout pinout;
-    
     pinout.red_led_pin = 16;
     pinout.yel_led_pin = 21;
     pinout.grn_led_pin = 19;
     pinout.btn_pin = 18;
 
+    // setting the handle to null
     TaskHandle_t xHandle = NULL;
 
+    // creating the task
     xTaskCreate( vTaskCode, "Led HIGH", STACK_SIZE, &pinout, tskIDLE_PRIORITY, &xHandle );
+    // if the button is pressed, the trigger will notify the ISRHandler to notify the task
     gpio_isr_handler_add( pinout.btn_pin, vISRHandler, ( void * )xHandle );
 }
 
 void app_main( void )
 {
+    // configures the gpio
     gpio_config( &red_led_io_conf );
     gpio_config( &yel_led_io_conf );
     gpio_config( &grn_led_io_conf );
     gpio_config( &btn_io_conf );
 
+    // starts the isr service
     gpio_install_isr_service(0);
 
+    //starts the taskBuilder
     taskBuilder();
 }
